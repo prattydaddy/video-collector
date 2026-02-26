97572fed07f16ad68646a1d724fbb44d7eb1c915
JoyBot <pratt.nate@gmail.com>
2026-02-26 02:44:07 +0000

    Add 27 Object Change pairs, bump STORE_VERSION to 4

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ import type { VideoPair, Stage } from "./types";
 import { STAGES } from "./types";
 
 const STORAGE_KEY = "video-collector-pairs";
-const STORE_VERSION = 3;
+const STORE_VERSION = 4;
 
 function loadPairs(): VideoPair[] {
   const version = localStorage.getItem("store-version");
