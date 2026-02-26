fcf32a8dcea0038c845e47d692fa64b5cf8b0423
JoyBot <pratt.nate@gmail.com>
2026-02-26 23:13:58 +0000

    debug: add env check endpoint

---
+++ api/debug-env.js
@@ -0,0 +1,11 @@
+export default async function handler(req, res) {
+  res.status(200).json({
+    hasClientId: !!process.env.GOOGLE_CLIENT_ID,
+    clientIdPrefix: (process.env.GOOGLE_CLIENT_ID || '').substring(0, 10),
+    hasClientSecret: !!process.env.GOOGLE_CLIENT_SECRET,
+    secretPrefix: (process.env.GOOGLE_CLIENT_SECRET || '').substring(0, 8),
+    hasRefreshToken: !!process.env.GOOGLE_REFRESH_TOKEN,
+    refreshPrefix: (process.env.GOOGLE_REFRESH_TOKEN || '').substring(0, 10),
+    hasDbUrl: !!process.env.DATABASE_URL,
+  });
+}
